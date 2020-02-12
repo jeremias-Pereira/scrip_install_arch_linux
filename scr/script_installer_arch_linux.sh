84c487e6e589a4d7858bb400f532e92f44453715
jeremias-Pereira <jeremiaspereira745@gmail.com>
2020-02-11 22:25:59 -0300

    new relese include interface whith dialog

--- scr/script_installer_arch_linux.sh
+++ scr/script_installer_arch_linux.sh
@@ -5,7 +5,7 @@
 #	scrip de instalacao do S.O ARCH linux
 #	Autor: Jeremias Pereira da silva
 #	Data:16/01/2020
-#	Versao: 0.2
+#	Versao: 1.0
 # 	Licensa: MIT								
 #
 #
@@ -25,26 +25,22 @@ PART_VAR=""
 PART_OPT=""
 PART_TMP=""
 
-echo -e "\033[32;1m Bem Vindo ao Instalador do Arch linux\033[m"
-echo 
-echo -e "\033[32;1m Discos indentificados no sistema\033[m"
+
+dialog --msgbox "Bem vindo ao Istalador Arch Linux" 
 #exibe os discos disponiveis no sistema para a instalacao
-fdisk -l |egrep "^Disk"|cut -d" " -f2|egrep ".{4}/[sh]"|sed "s/://g"
-echo -e "\033[31;1m Sao necessarios no minimo 2 particoes \033[m"
-echo -e "\033[31;1m Por padrao a primeira particao sera usada pra alocar o bootloader\033[m"
-echo -e "\033[31;1m E recomendado cria a primeira particao com no minimo 512MB\033[m"
-echo -e "\033[32;1m Qual disco voce quer usar para instalar seu novo SO Arch\033[m"
-read DISK
-#atribui o diretorio barra boot como a primeira particao do disco
-PART_BOOT="${DISK}1"
+DISK=`dialog --stdout --menu "Escola um disco para instalar o Arch" 0 0 0 $(fdisk -l |grep "^Disk /dev/sd."|cut -d" " -f2|sed "s/:/ DISK/g")`
+dialog --msgbox  "Sao necessarios no minimo 2 particoes\
+       Por padrao a primeira particao sera usada pra alocar o bootloader\
+       E recomendado cria a primeira particao com no minimo 512MB" 50 50
 
 #verificando se o disco informado para a instalacao existe
 TEST=`fdisk -l |grep "${DISK}"`
 if [  -n "${TEST}" ]
 	then
 		clear
-		echo -e "\033[33;1m Obs: atribua a flag \033[31;1m Bootable \033[33;1m A primeira particao do disco ao entrar no particionador\033[m"
-		sleep 3 
+		dialog --msgbox "Obs: atribua a flag Bootable\
+				A primeira particao do disco ao\
+				Entrar no particionador" 50 50
 
 		cfdisk "${DISK}"
 		clear
@@ -73,28 +69,24 @@ if [  -n "${TEST}" ]
 			mkfs.ext4 "$PART_RAIZ"
 		fi
 else
-	echo -e "\033[31;1m Disco Nao encontrado\033[m"
+	dialog --msgbox "DISCO NAO ENCONTRADO" 0 0
 	exit 0
 fi
 
 if [ -z "${PART_RAIZ}" ]
 	then
 		for i in $(seq ${COUNT_PARTITION}) ;
-			do
+			do	
+				#if [ -z ${PART_RAIZ} ]
+				#then
 				if [ $i -gt 1 ] 
 				then
 
         			echo -e "\033[32;1m Qual o ponto de montagem da particao \n\n\033[31;1m${DISK}${i}\033[m"
-				echo -e "\033[32;1m Pontos de montagem posiveis que podem ser montados fora do /(raiz)\033[33;1m\
-					\n / \
-					\n /root\
-					\n /usr\
-					\n /home\
-					\n /var\
-					\n /opt\
-					\n /tmp\033[m"
-        			read DISK_PART
-				clear
+				dialog --msgbox "Defina primeiramente um ponto de mountagem para a particao raiz e obrigatorio" 50 50 
+				DISK_PART=`dialog --stdout --menu \
+					"PONTOS DE MONTAGEM DISPONIVEIS" 25 50 7 / . $(ls /|egrep "^[\\rovthu][opsa]"|sed "s/^/\//g"|sed "s/$/ ./g" )`
+								
 				case "${DISK_PART}" in
 					    '/') PART_RAIZ=${DISK}${i};;
 				        '/root') PART_ROOT=${DISK}${i};;
@@ -117,42 +109,41 @@ mount "${PART_RAIZ}" /mnt
 mkdir -p /mnt/boot
 mount "${PART_BOOT}" /mnt/boot
 
-if [ -z "${PART_HOME}" ]
+if [ -n "${PART_HOME}" ]
 then
 	mkdir -p /mnt/home
 	mount "${PART_HOME}" /mnt/home
 fi
-if [ -z "${PART_ROOT}" ]
+if [ -n "${PART_ROOT}" ]
 then
 	mkdir -p /mnt/root
 	mount "${PART_ROOT}" /mnt/root
+	echo "HELLO WORD"
 fi
-if [ -z "${PART_USR}" ]
+if [ -n "${PART_USR}" ]
 then
 	mkdir -p /mnt/usr
 	mount "${PART_USR}" /mnt/usr
 fi
-if [ -z "${PART_VAR}" ]
+if [ -n "${PART_VAR}" ]
 then
 	mkdir -p /mnt/var
 	mount "${PART_VAR}" /mnt/var
 fi
-if [ -z "${PART_OPT}" ]
+if [ -n "${PART_OPT}" ]
 then
 	mkdir -p /mnt/opt
 	mount "${PART_OPT}" /mnt/opt
 fi
-if [ -z "${PART_TMP}" ]
+if [ -n "${PART_TMP}" ]
 then
 	mkdir -p /mnt/tmp
 	mount "${PART_TMP}" /mnt/tmp
 fi
 
 pacstrap /mnt base linux linux-firmware
-
 #gerando o arquivo fstab
-genfstab -U /mnt >> /mnt/etc/fstab
-
+genfstab -U /mnt >> /mnt/etc/fstab 
 #copiando o segundo script de instalacao para o novo sistema
 cp ./script2_installer_arch_linux.sh /mnt
 arch-chroot /mnt
